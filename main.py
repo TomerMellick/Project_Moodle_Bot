0c4986b53c17f7d72d9d62c4087a44a2b11292ca
Tomer_Mellick <tomer735@gmail.com>
2022-08-10 22:06:33 +0300

    renaming all the main veraibels

--- main.py
+++ main.py
@@ -1,34 +1,56 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import requests
-
 import re
 import json
 
-s = requests.Session()
-r = s.get("https://live.or-bit.net/hadassah/Login.aspx")
+# getting the bot token from file
+with open("BotToken.txt", "r") as BotToken_f:
+    BotToken = BotToken_f.read()
+telegram_url = f"https://api.telegram.org/bot{BotToken}/"
 
-ans = re.findall(r"<input type=\"hidden\" name=\"(.*?)\" id=\".*?\" value=\"(.*?)\" \/", r.text)
-ans.append(("edtUsername", "{}"))
-ans.append(("edtPassword", "{}"))
-ans += [("__LASTFOCUS", ""), ("__EVENTTARGET", ""), ("__EVENTARGUMENT", ""), ("btnLogin", "כניסה")
-        ]
-ans = dict(ans)
-r = s.post("https://live.or-bit.net/hadassah/Login.aspx", data=ans)
-r = s.get("https://live.or-bit.net/hadassah/Handlers/Moodle.ashx")
-ans = re.search("URL='(.*?)'", r.text)[1]
-r = s.get(ans)
-ans = re.search('"sesskey":"(.*?)"', r.text)[1]
-string_url = f"https://mowgli.hac.ac.il/lib/ajax/service.php?sesskey={ans}&info=core_calendar_get_action_events_by_timesort"
-r = s.post(string_url, json=[{"index": 0, "methodname": "core_calendar_get_action_events_by_timesort",
-                              "args": {"limitnum": 6, "timesortfrom": 1658869200, "limittononsuspendedevents": True}}])
-telegram_url = "https://api.telegram.org/bot{}/"
-heb_txt = json.loads(r.text)
+# getting the username and password from database
+with open("DataBase.txt", "r") as DataBase_f:
+    user_info = DataBase_f.read()
+    user_info = json.loads(user_info)
+user_id =  #TODO getting from telegream
+user_name = user_info[user_id]["user_name"]
+user_password = user_info[user_id]["password"]
+
+# open session
+s = requests.Session()
+
+# GET orbit login page
+orbit_session = s.get("https://live.or-bit.net/hadassah/Login.aspx")
+
+# setting the POST headrs for the orbit login
+login_POST_header = re.findall(r"<input type=\"hidden\" name=\"(.*?)\" id=\".*?\" value=\"(.*?)\" \/",
+                               orbit_session.text)
+login_POST_header.append((f"edtUsername", user_name))
+login_POST_header.append((f"edtPassword", user_password))
+login_POST_header += [("__LASTFOCUS", ""), ("__EVENTTARGET", ""), ("__EVENTARGUMENT", ""), ("btnLogin", "כניסה")]
+login_POST_header = dict(login_POST_header)
+
+# POST login to orbit
+orbit_session = s.post("https://live.or-bit.net/hadassah/Login.aspx", data=login_POST_header)
+
+# GET redirect to moodle
+moodle_session = s.get("https://live.or-bit.net/hadassah/Handlers/Moodle.ashx")
+redirect_url = re.search("URL='(.*?)'", moodle_session.text)[1]
+moodle_session = s.get(redirect_url)
+
+# POST to unfinished events
+moodle_session_key = re.search('"sesskey":"(.*?)"', moodle_session.text)[1]
+events_POST_url = f"https://mowgli.hac.ac.il/lib/ajax/service.php?sesskey={moodle_session_key}&info=core_calendar_get_action_events_by_timesort"
+events_POST_headers = {"index": 0, "methodname": "core_calendar_get_action_events_by_timesort",
+                              "args": {"limitnum": 6, "timesortfrom": 1658869200, "limittononsuspendedevents": True}}
+unfinished_events = s.post(events_POST_url, json=[events_POST_headers])
+
+# encoding the unicode response to hebrew
+heb_txt = json.loads(unfinished_events.text)
 heb_txt = heb_txt[0]['event']['data']
-id =
-txt_string = heb_txt[0]['name']
-txt_string = txt_string.replace('#', '%23%0A')
-course_name =
-s.get(f"{telegram_url}sendMessage?chat_id={id}&text={txt_string}")
+
+# building the message string
+message_string = heb_txt[0]['name']
+message_string = message_string.replace('#', '%23%0A')
+
+# send telegram message
+s.get(f"{telegram_url}sendMessage?chat_id={user_id}&text={message_string}")
